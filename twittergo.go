6891b3fe4d60424d37918afba96e2a028fc06506
Arne Roomann-Kurrik <kurrik@twitter.com>
2012-09-11 15:23:48 -0700

    Add a package description

--- twittergo.go
+++ twittergo.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Implements a Twitter client library in Go.
 package twittergo
 
 import (
